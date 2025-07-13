4faa3c51fbd2d3da0cde0045fc6efbcf7a046e1c
PunithNaveen2003 <1924naveen@gmail.com>
2025-07-13 15:59:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*  Typing animation */
 var typed = new Typed(".typing",{
-    strings:["","UI/UX Designer","Web Designer"],
+    strings:["","UI/UX Designer","Web Designer","Graphic Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
